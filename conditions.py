229f58508a6e28a883ab42a5b97f0f67c232ab23
Jan Kašpar <jan.kaspar@cern.ch>
2019-12-12 17:19:21 +0100

    Added choice of conditions from GT.

--- conditions.py
+++ conditions.py
@@ -10,6 +10,16 @@ def UseLHCInfoLocal(process):
   global lhcInfoDefined
   lhcInfoDefined = True
 
+
+def UseLHCInfoGT(process):
+  global lhcInfoDefined
+  lhcInfoDefined = True
+
+  # these are not defined in local environment by default
+  #del process.ctppsRPLHCInfoCorrectionsDataESSourceXML
+  #del process.esPreferLocalLHCInfo
+
+
 def UseLHCInfoFile(process, connection, tag):
   global lhcInfoDefined
   lhcInfoDefined = True
@@ -37,6 +47,15 @@ alignmentDefined = False
 def UseAlignmentLocal(process):
   global alignmentDefined
   alignmentDefined = True
+
+
+def UseAlignmentGT(process):
+  global alignmentDefined
+  alignmentDefined = True
+
+  del process.ctppsRPAlignmentCorrectionsDataESSourceXML
+  del process.esPreferLocalAlignment
+
 
 def UseAlignmentFile(process, connection, tag):
   global alignmentDefined
@@ -86,6 +105,15 @@ def UseOpticsLocal(process):
   global opticsDefined
   opticsDefined = True
 
+
+def UseOpticsGT(process):
+  global opticsDefined
+  opticsDefined = True
+
+  del process.ctppsOpticalFunctionsESSource
+  del process.esPreferLocalOptics
+
+
 def UseOpticsFile(process, connection, tag):
   global opticsDefined
   opticsDefined = True
@@ -104,6 +132,7 @@ def UseOpticsFile(process, connection, tag):
   )
   
   process.esPreferDBFileOptics = cms.ESPrefer("PoolDBESSource", "PoolDBESSourceOptics")
+
 
 def UseOpticsDB(process, connection, tag):
   global opticsDefined
@@ -130,25 +159,26 @@ def UseOpticsDB(process, connection, tag):
 def SetConditions(process):
   # chose global tag
   process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-  process.GlobalTag = GlobalTag(process.GlobalTag, "110X_dataRun2_v5")
-  #process.GlobalTag = GlobalTag(process.GlobalTag, "106X_dataRun2_v11")
-  #process.GlobalTag = GlobalTag(process.GlobalTag, "106X_dataRun2_testPPS_v1")
-  #process.GlobalTag = GlobalTag(process.GlobalTag, "106X_dataRun2_v10")
+  #process.GlobalTag = GlobalTag(process.GlobalTag, "106X_dataRun2_2017_2018_Candidate_2019_12_02_18_57_30")
+  process.GlobalTag = GlobalTag(process.GlobalTag, "106X_dataRun2_v24")
   #process.GlobalTag = GlobalTag(process.GlobalTag, "auto:run2_data")
 
   # chose LHCInfo
-  UseLHCInfoLocal(process)
+  UseLHCInfoGT(process)
+  #UseLHCInfoLocal(process)
   #UseLHCInfoDB(process, "frontier://FrontierProd/CMS_CONDITIONS", "LHCInfoEndFill_prompt_v2")
 
   # chose alignment
-  UseAlignmentLocal(process)
+  #UseAlignmentGT(process)
+  #UseAlignmentLocal(process)
   #UseAlignmentFile(process, "sqlite_file:/afs/cern.ch/user/c/cmora/public/CTPPSDB/AlignmentSQlite/CTPPSRPRealAlignment_v13Jun19_v1.db", "PPSRPRealAlignment_v13Jun19")
-  #UseAlignmentDB(process, "frontier://FrontierProd/CMS_CONDITIONS", "CTPPSRPAlignment_real_offline_v5")
+  UseAlignmentDB(process, "frontier://FrontierProd/CMS_CONDITIONS", "CTPPSRPAlignment_real_offline_v7")
 
   # chose optics
-  UseOpticsLocal(process)
+  #UseOpticsGT(process)
+  #UseOpticsLocal(process)
   #UseOpticsFile(process, "sqlite_file:/afs/cern.ch/user/w/wcarvalh/public/CTPPS/optical_functions/PPSOpticalFunctions_2016-2018_v7.db", "PPSOpticalFunctions_test")
-  #UseOpticsDB(process, "frontier://FrontierProd/CMS_CONDITIONS", "PPSOpticalFunctions_offline_v5")
+  UseOpticsDB(process, "frontier://FrontierProd/CMS_CONDITIONS", "PPSOpticalFunctions_offline_v6")
 
   # check choices
   if not lhcInfoDefined:
